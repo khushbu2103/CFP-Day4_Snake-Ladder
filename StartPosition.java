1d806f0fb63c4bc42cb1d326e504abdf4ee97950
khushbu <khushbusoni475@gmail.com>
2023-12-06 14:01:23 +0530

    [khushbu] modify snake ladder game start position

---
+++ StartPosition.java
@@ -0,0 +1,15 @@
+package Day4_SnakeAndLadder_PP;
+
+import java.util.Scanner;
+
+public class UC1_StartPosition {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("Press anything number start the game");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Welcome to snake and ladder game");
+        int startPosition = 0;
+    }
+}
--- UC1_StartPosition.java
+++
@@ -1,7 +0,0 @@
-package Day4_SnakeAndLadder_PP;
-
-public class UC1_StartPosition {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
