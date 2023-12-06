cae420c85cad83b0a96715728de9232971c6761b
khushbu <khushbusoni475@gmail.com>
2023-12-06 17:02:13 +0530

    [khushbu] modify snake ladder game with two player

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -11,43 +11,103 @@ public class SnakeAndLadderGame {
         int x = sc.nextInt();
         System.out.println("Welcome to snake and ladder game");
         int pl1_Position = 0;
+        int pl2_Position = 0;
         int pl1_numberOfDiceRoll = 0;
-        while (pl1_Position < 100)
+        int pl2_numberOfDiceRoll = 0;
+        int turn = 1;
+        Random random = new Random();
+        while (turn == 1 || turn == 2 )
         {
-            Random random = new Random();
+            System.out.println("==================");
             int pl1_DiceRollNumber = random.nextInt(6);
-            pl1_numberOfDiceRoll++;
-            System.out.println("Player rolls the dice and get " + pl1_DiceRollNumber + " number");
-            int pl1_Option = random.nextInt(3);// 0,1,2
-            switch (pl1_Option)
+            int pl2_DiceRollNumber = random.nextInt(6);
+            if(turn == 1)
             {
-                case 0:
-                    System.out.println("No play");
-                    break;
-                case 1:
-                    pl1_Position = pl1_Position + pl1_DiceRollNumber;
-                    System.out.println("Ladder");
-                    break;
-                case 2:
-                    pl1_Position = pl1_Position - pl1_DiceRollNumber;
-                    System.out.println("Snake");
-                    break;
-            }
-            if(pl1_Position < 0)
-            {
-                pl1_Position = 0;
-            } else if (pl1_Position >100)
-            {
-                pl1_Position = 100;
-            }
-            System.out.println("position of player one is: " + pl1_Position);
+                pl1_numberOfDiceRoll++;
+                System.out.println("Player 1 rolls the dice and get " + pl1_DiceRollNumber + " number");
+                int pl1_Option = random.nextInt(3);// 0,1,2
+                switch (pl1_Option)
+                {
+                    case 0:
+                        System.out.println("No play");
+                        break;
+                    case 1:
+                        pl1_Position = pl1_Position + pl1_DiceRollNumber;
+                        System.out.println("Ladder");
+                        break;
+                    case 2:
+                        pl1_Position = pl1_Position - pl1_DiceRollNumber;
+                        System.out.println("Snake");
+                        break;
+                }
+                if(pl1_Position < 0)
+                {
+                    pl1_Position = 0;
+                }
+                else if (pl1_Position > 100)
+                {
+                    pl1_Position = 100;
+                }
+                System.out.println("position of player 1 is: " + pl1_Position);
 
-            if(pl1_Position == 100)
-            {
-                System.out.println("player one has won");
+                if(pl1_Position == 100)
+                {
+                    turn = 0;
+                    System.out.println("player 1 has won after " + pl1_numberOfDiceRoll + " rolls");
+                }
+                else
+                {
+                    if (pl1_Option != 1)
+                    {
+                        turn = 2;
+                    }
+                }
             }
 
+            else if(turn == 2)
+            {
+                pl2_numberOfDiceRoll++;
+                System.out.println("Player 2 rolls the dice and get " + pl1_DiceRollNumber + " number");
+                int pl2_Option = random.nextInt(3);// 0,1,2
+                switch (pl2_Option)
+                {
+                    case 0:
+                        System.out.println("No play");
+                        break;
+                    case 1:
+                        pl2_Position = pl2_Position + pl2_DiceRollNumber;
+                        System.out.println("Ladder");
+                        break;
+                    case 2:
+                        pl2_Position = pl2_Position - pl2_DiceRollNumber;
+                        System.out.println("Snake");
+                        break;
+                }
+                if(pl2_Position < 0)
+                {
+                    pl2_Position = 0;
+                }
+                if (pl2_Position > 100)
+                {
+                    pl2_Position = 100;
+                }
+                System.out.println("position of player two is: " + pl2_Position);
+
+                if(pl2_Position == 100)
+                {
+                    turn = 0;
+                    System.out.println("player 2 has won after " + pl2_numberOfDiceRoll + " rolls");
+                }
+                else
+                {
+                    if (pl2_Option != 1)
+                    {
+                        turn = 1;
+                    }
+                }
+            }
+            System.out.println("XXXXXXXXXXXXXXXXXX");
         }
-        System.out.println(pl1_numberOfDiceRoll + " time dice rolled to won the game");
+
     }
 }
