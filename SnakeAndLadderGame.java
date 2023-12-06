f48ac43f0b8ab3d4f039c6ccadc9143caa81728e
khushbu <khushbusoni475@gmail.com>
2023-12-06 14:34:41 +0530

    [khushbu] modify snake ladder game player rolls the dice

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,8 +1,9 @@
 package Day4_SnakeAndLadder_PP;
 
+import java.util.Random;
 import java.util.Scanner;
 
-public class StartPosition {
+public class SnakeAndLadderGame {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Press anything number start the game");
@@ -10,5 +11,10 @@ public class StartPosition {
         int x = sc.nextInt();
         System.out.println("Welcome to snake and ladder game");
         int startPosition = 0;
+
+        //Uc-2
+        Random random = new Random();
+        int numberOfRoll = random.nextInt(6);
+        System.out.println("Player rolls the dice and get " + numberOfRoll + " number");
     }
 }
